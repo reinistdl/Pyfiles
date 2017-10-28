8f8ce08653afc1c71ebadb3161120002ada13cb7
Reinis Vate <reinis.vate@testdevlab.com>
2017-10-28 15:16:40 +0300

    Improved output format for commision amounts.

--- commision_removal.py
+++ commision_removal.py
@@ -5,9 +5,9 @@ val2=sys.argv[2]
 def calcuclate_commision(price=val1, amount=val2):
     one_unit_commision=float(price)
     one_unit_commision=one_unit_commision*float(0.0025)
-    total_commision_for_transaction=float(one_unit_commision)*float(amount)
+    total_commision_for_transaction=(float(one_unit_commision)*float(amount))
 
-    print "One unit commision: %.6f \nTotal commision for transaction: %.6f \nPrice increase required: %.6f" % (one_unit_commision,total_commision_for_transaction, one_unit_commision*2)
+    print "One unit commision: %.4f Cents \nTotal commision for transaction: %.2f Euros \nPrice increase required: %.6f" % (one_unit_commision*100,total_commision_for_transaction, one_unit_commision*2*100)
 
 if __name__ == '__main__':
     calcuclate_commision()
